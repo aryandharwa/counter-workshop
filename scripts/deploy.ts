3d25354131f9a2ae6797d1b4a471529209f9d9ad
Aryan Dharwa <adharwa2002@gmail.com>
2024-09-19 22:27:04 +0530

    assignment complete, smart contract deployed

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -20,9 +20,7 @@ async function main() {
   let sierraCode, casmCode;
 
   try {
-    ({ sierraCode, casmCode } = await getCompiledCode(
-      "workshop_counter_contract"
-    ));
+    ({ sierraCode, casmCode } = await getCompiledCode("workshop_counter_contract"));
   } catch (error: any) {
     console.log("Failed to read contract files");
     process.exit(1);
@@ -30,17 +28,22 @@ async function main() {
 
   const myCallData = new CallData(sierraCode.abi);
   const constructor = myCallData.compile("constructor", {
-    initial_value: 100,
-    kill_switch:
+    init_value: 100,
+    kill_switch_address:
       "0x05f7151ea24624e12dde7e1307f9048073196644aa54d74a9c579a257214b542",
     initial_owner: process.env.DEPLOYER_ADDRESS ?? "",
   });
+
+  console.log("after constructor", { constructor })
+
   const deployResponse = await account0.declareAndDeploy({
     contract: sierraCode,
     casm: casmCode,
     constructorCalldata: constructor,
     salt: stark.randomAddress(),
   });
+
+  console.log("Contract deployed\n");
 
   // Connect the new contract instance :
   const myTestContract = new Contract(
